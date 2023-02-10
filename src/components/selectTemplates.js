19cf3964cd086a20317d3308e1c6ce493f01aa48
Sreeraagh K <sreeraaghk@Sree-Macbook-Pro.local>
2023-02-11 02:17:29 +0530

    a

--- src/components/selectTemplates.js
+++ src/components/selectTemplates.js
@@ -5,6 +5,7 @@ export default function Templates(props) {
 
 
   return (props.trigger)?(
+    
     <>
     <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
