64545eacd4d9bdf5b215fd663bdf1c4da2995cf3
chKrishnavamsi <chimatakrishnavamsi@gmail.com>
2023-02-24 02:33:30 +0530

    UI Hyderation Error Fix, Minor UI modifications

--- src/components/uploadCSV.js
+++ src/components/uploadCSV.js
@@ -5,6 +5,8 @@ import LeadTable from "./leadtable";
 const rowsArray = [];
 const valuesArray = [];
 
+
+
 export default function UploadCSV() {
   const { CSVReader } = useCSVReader();
   const { readString } = usePapaParse();
@@ -29,69 +31,59 @@ export default function UploadCSV() {
     }
   };
 
+
+
   return (
     <>
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <CSVReader
-        onUploadAccepted={(results) => {
-          // Iterating data to get column name and their values
-          results.data.map((data) => {
-            rowsArray.push(Object.values(data));
-            valuesArray.push(Object.values([data]));
-          });
-          
-          setValues(valuesArray);
-          
-          // Parsed Data Response in array format
-          setParsedData(results.data);
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <CSVReader
+          onUploadAccepted={(results) => {
+            // Iterating data to get column name and their values
+            results.data.map((data) => {
+              rowsArray.push(Object.values(data));
+              valuesArray.push(Object.values([data]));
+            });
 
-          // Filtered Column Names
-          setTableRows(rowsArray[0]);
+            setValues(valuesArray);
 
-          // Filtered Values
-        }}
-      >
-        {({ getRootProps, acceptedFile, ProgressBar, getRemoveFileProps }) => (
-          <>
-            <div>
-              <div className="mt-16 px-4 sm:px-6 lg:px-8">
-                <div className="sm:flex sm:items-center">
-                  <div className="sm:flex-auto">
-                    <h1 className="text-xl font-semibold text-gray-900">
-                      Users
-                    </h1>
-                    <p className="mt-2 text-sm text-gray-700">
-                      A list of all the users in your account including their
-                      name, title, email and role.
-                    </p>
-                  </div>
-                  <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                    <button
-                      type="button"
-                      {...getRootProps()}
-                      className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                    >
-                      Browse file
-                    </button>
-                    <div>{acceptedFile && acceptedFile.name}</div>
-                    <button {...getRemoveFileProps()}>Remove</button>
+            // Parsed Data Response in array format
+            setParsedData(results.data);
+
+            // Filtered Column Names
+            setTableRows(rowsArray[0]);
+
+            // Filtered Values
+          }}
+        >
+          {({ getRootProps, acceptedFile, ProgressBar, getRemoveFileProps }) => (
+            <>
+              <div>
+                <div className="mt-16 px-4 sm:px-6 lg:px-8">
+                  <div className="sm:flex sm:items-center">
+                    <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                      <button
+                        type="button"
+
+                        {...getRootProps()}
+                        className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                      >
+                        Browse file
+                      </button>
+                      <div>{acceptedFile && acceptedFile.name}</div>
+                      <button className="inline-flex items-center mt-4 justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto" 
+                        onClick={submitData} {...getRemoveFileProps()}>Submit</button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <ProgressBar />
-            <LeadTable valuesArray={valuesArray} />
+              <ProgressBar />
 
-            <button
-              onClick={submitData}
-              className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              UploadCSV
-            </button>
-          </>
-        )}
-      </CSVReader>
-    </div>
+
+
+            </>
+          )}
+        </CSVReader>
+      </div>
     </>
   );
 }
--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -6,11 +6,7 @@ export default function Contacts() {
   return (
     <div>
          <SidebarHeader />
-      <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
-        <UploadCSV />
-        
-
-      </div>
+      
     </div>
   )
 }
