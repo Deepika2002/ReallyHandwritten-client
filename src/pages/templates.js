a6796d8ddfd9e93be8c3855abdebf824df83d4b5
chKrishnavamsi <chimatakrishnavamsi@gmail.com>
2023-03-07 23:52:22 +0530

    Templates Page

--- src/pages/templates.js
+++ src/pages/templates.js
@@ -75,7 +75,7 @@ export default function Templates() {
     <div className="flex-col lg:pl-64 bg-white">
       <div className="flex-1 mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="flex items-center justify-between space-x-4">
-          <h2 className="text-lg font-medium text-gray-900">Customers also viewed</h2>
+          <h2 className="text-lg font-medium text-gray-900">Templates</h2>
           <a href="#" className="whitespace-nowrap text-sm font-medium text-indigo-600 hover:text-indigo-500">
             View all
             <span aria-hidden="true"> &rarr;</span>
@@ -87,9 +87,9 @@ export default function Templates() {
               <div className="aspect-w-4 aspect-h-3 overflow-hidden rounded-lg bg-gray-100">
                 <img src={product.imageSrc} alt={product.imageAlt} className="object-cover object-center" />
                 <div className="flex items-end p-4 opacity-0 group-hover:opacity-100" aria-hidden="true">
-                  <div className="w-full rounded-md bg-white bg-opacity-75 py-2 px-4 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter">
+                  <button className="w-full rounded-md bg-white bg-opacity-75 py-2 px-4 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter">
                     View Product
-                  </div>
+                  </button>
                 </div>
               </div>
               <div className="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-gray-900">
