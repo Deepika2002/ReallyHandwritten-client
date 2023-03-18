6810df32399004f95b3ca11ee6184482448e9704
chKrishnavamsi <chimatakrishnavamsi@gmail.com>
2023-03-18 18:13:21 +0530

    Home Page design changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -505,7 +505,7 @@ export default function Home() {
               alt=""
               height="1024"
               width="1024"
-              className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:w-full md:w-full md:mx:auto sm:mt-10 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+              className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:w-full md:w-full md:mx:auto sm:mt-6 lg:mt-6 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
             />
         </div>
       </div>
