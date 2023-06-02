a04822707805b96f07421af350ff1bf7bc6991c0
chKrishnavamsi <chimatakrishnavamsi8padma@gmail.com>
2023-06-03 02:31:40 +0530

    contact to userpreference and pulling them in view

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,12 +1,11 @@
 
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "../../../../prisma/prisma"
 import bcrypt from "bcrypt"
 
-export default NextAuth({
+const options ={
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
@@ -14,18 +13,11 @@ export default NextAuth({
   providers: [
     
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
      
       async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
         const {email,password}= credentials;
-        // console.log({email,password});
 
         const user = await prisma.user.findUnique({
           where: { email }
@@ -41,29 +33,22 @@ export default NextAuth({
         }
   
         if (user && isMatch) {
-          // Any object returned will be saved in `user` property of the JWT
-          // console.log(user)
-
-          // console.log("user details",user)
+          
           return {
             ...user,
             id: user.id,
             
           };
         } else {
-          // If you return null then an error will be displayed advising the user to check their details.
           return null
-  
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+
         }
       }
     }),
   ],
   callbacks: {
     async session({ session, token }) {
-      // console.log('session', { session, token })
       if (token && token.id) {
-        // console.log("token id",token.id)
         session.user.id = token.id;
         session.user.role = token.role;
         session.user.verificationCode = token.verificationCode
@@ -75,7 +60,6 @@ export default NextAuth({
       return session;
     },
     async jwt({ token, user }) {
-      // console.log('jwt', { token, user })
       if (token && user && user.id) {
         token.id = user.id;
         token.role = user.role;
@@ -84,8 +68,6 @@ export default NextAuth({
       return token;
     }
   }
-  
-  
-  
-});
 
+}
+export default (req, res) => NextAuth(req, res, options)
